77b4ad736840cd66021cad5758fc55aee42067b7
Abrilam <abril.morenomaltez@alu.frt.utn.edu.ar>
2026-04-18 22:12:43 -0300

    Actualizacion de clases asociadas

--- src/main/java/app/Program.java
+++ src/main/java/app/Program.java
@@ -1,12 +1,10 @@
 package app;
 
-import data.Persistencia;
 import java.util.InvalidPropertiesFormatException;
 import views.ListarVehiculosView;
 
 public class Program {
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
-        Persistencia.inicializar();
         ListarVehiculosView view = new ListarVehiculosView();
         view.setVisible(true);
     }
--- src/main/java/data/Persistencia.java
+++ src/main/java/data/Persistencia.java
@@ -9,43 +9,6 @@ public class Persistencia {
     private static ArrayList<Responsable> responsables = new ArrayList<>();
     private static ArrayList<Sucursal> sucursales = new ArrayList<>();
     
-    private static void inicializarResponsables(){
-        Responsable r1 = new Responsable("Carlos Gómez", "25444111", "3815551111");
-        Responsable r2 = new Responsable("Laura Pérez", "30111222", "3815552222");
-        responsables.add(r1);
-        responsables.add(r2);
-    }
-    
-    private static void inicializarSucursales(){
-        Sucursal s1 = new Sucursal("SUC01", "Av. Belgrano 1200", "Tucumán", responsables.get(0));
-        Sucursal s2 = new Sucursal("SUC02", "San Martín 450", "Yerba Buena", responsables.get(1));
-        
-        sucursales.add(s1);
-        sucursales.add(s2);
-    }
-    
-    private static void inicializarVehiculos(){
-        
-        Sucursal s1 = sucursales.get(0);
-        Sucursal s2 = sucursales.get(1);
-        
-        Marca m1 = new Marca("Renault", "Francia");
-        Marca m2 = new Marca("Ford", "EEUU");
-        Marca m3 = new Marca("Iveco", "Italia");
-        Marca m4 = new Marca("Mercedes", "Alemania");
-        
-        VehiculoElectrico v1 = new VehiculoElectrico("AE123FG", m1, "Kangoo E-Tech", 2020, 1000, s1, 16);
-        VehiculoElectrico v2 = new VehiculoElectrico("AF456HI", m2, "E-Transit", 2021, 1300, s2, 16);
-
-        VehiculoCombustible v3 = new VehiculoCombustible("AC789JK", m3, "Daily", 2023, 1200, s1, 8, 1.5);
-        VehiculoCombustible v4 = new VehiculoCombustible("AD321LM", m4, "Sprinter", 2020, 1200, s2, 7, 1);
-        
-        vehiculos.add(v1);
-        vehiculos.add(v2);
-        vehiculos.add(v3);
-        vehiculos.add(v4);
-    }
-    
     public static ArrayList<Vehiculo> getVehiculos(){
         return vehiculos;
     }
@@ -56,9 +19,5 @@ public class Persistencia {
                 .findFirst();
     }
     
-    public static void inicializar(){
-        inicializarResponsables();
-        inicializarSucursales();
-        inicializarVehiculos();
-    }
+   
 }
